26abb8eed6735ad2c11f75815f400bd527be9837
EdsonJunio <edsonbh97@gmail.com>
2025-03-24 22:22:34 -0300

    go

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,19 +2,23 @@ package controller
 
 import (
 	"fmt"
-	"github.com/EdsonJunio/crud-go/src/configuration/rest_err"
+	"github.com/EdsonJunio/crud-go/src/configuration/validation"
 	"github.com/EdsonJunio/crud-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func CreateUser(c *gin.Context) {
-
+	log.Println("Init CreateUser Controller")
 	var userRequest request.UserResquest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadResquestErro(
-			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
-		c.JSON(restErr.Code, restErr)
+		log.Println("Error trying to marshal object, error%s\n", err.Error())
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
 		return
 	}
+
+	fmt.Println(userRequest)
 }
